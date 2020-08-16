76a2149b610a9629b4586284c8d3a635cd18cb88
awahle1 <austinwahle1@gmail.com>
2020-08-16 19:39:29 -0400

    Images working for real

--- lunchroom_app/lunch/urls.py
+++ lunchroom_app/lunch/urls.py
@@ -32,5 +32,5 @@ urlpatterns = [
     path('propic', views.propic, name='propic'),
     ]
 
-#if settings.DEBUG:
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
